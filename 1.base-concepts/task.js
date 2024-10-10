2f9b991570f73521c217560ff415bfcbea086ee6
Elina test <elina.ulli93@gmail.com>
2024-10-10 07:28:14 +0500

    Первое задание исправлено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,10 +4,7 @@ function solveEquation(a, b, c) {
 	let arr = [];
 	let discriminant = b ** 2 - (4 * a * c);
 
-	if (discriminant < 0) {
-		arr = [];
-
-	} else if (discriminant === 0) {
+ if (discriminant === 0) {
 		arr[0] = -b / (2 * a);
 
 	} else if (discriminant > 0) {
@@ -22,16 +19,14 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	let percentOfMonth; //месячная ставка
 	let monthlyPayment; // ежемесячный платеж
 	let totalPay; //общая сумма кредита строкое значение
-	let totalPayResualt; // результат в цисловом значение  
+	// let totalPayResualt; // результат в цисловом значение  
 
 	percentOfMonth = (percent / 100) / 12; // ежемесячная ставка
 	loanBody = amount - contribution;
 	monthlyPayment = loanBody * (percentOfMonth + (percentOfMonth / (((1 + percentOfMonth) ** countMonths) - 1)));
 	totalPay = monthlyPayment * countMonths;
 
-	totalPayResualt = Number(totalPay.toFixed(2));
-
-	return totalPayResualt;
+	return Number(totalPay.toFixed(2))
 
 
 
